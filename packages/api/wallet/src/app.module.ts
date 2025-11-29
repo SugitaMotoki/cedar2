87326fa1495ce6b524e92b04e562847b80ee1a6f
SugitaMotoki <ee197011@meiji.ac.jp>
2025-11-29 14:02:47 +0900

    turboで実行可能にする

--- packages/api/wallet/src/app.module.ts
+++ packages/api/wallet/src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        ".env", // walletの.envファイル（優先）
+        join("..", "..", "..", ".env"), // ルートの.envファイル
+      ],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/api/wallet/src/main.ts
+++ packages/api/wallet/src/main.ts
@@ -1,8 +1,40 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
+/**
+ * 環境変数を取得する関数
+ * @param key 環境変数名
+ * @returns 値
+ */
+const getEnv = (key: string): string => {
+  const value = process.env[key];
+  if (value === undefined || value == "") {
+    throw new Error(`環境変数 "${key}" を取得できませんでした。`);
+  }
+  return value;
+};
+
+/**
+ * アプリを起動する関数
+ */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const HOST_NAME: string = getEnv("HOSTNAME");
+  const PORT = {
+    WALLET: getEnv("WALLET_PORT"),
+    CEDAR_PORTAL: getEnv("CEDAR_PORTAL_PORT"),
+  } as const;
+
+  app.enableCors({
+    origin: [
+      `http://${HOST_NAME}:${PORT.CEDAR_PORTAL}`,
+      `https://${HOST_NAME}:${PORT.CEDAR_PORTAL}`,
+    ],
+  });
+
+  await app.listen(PORT.WALLET);
 }
-bootstrap();
+
+void bootstrap().then();
